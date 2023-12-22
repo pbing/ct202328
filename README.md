68d010cfc010f9d2983207919d2057ac6632c8e0
Bernd Beuster <bernd.beuster@gmail.com>
2023-12-22 12:13:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Breaking Vigenère chiffre with Kasiski test in Haskell
+
+## Installation
+
+```shell
+git clone https://github.com/pbing/ct202328.git
+cd ct202328
+ghci kasiski.hs
+```
+
+## Reference
+* [Wilhelm Drehling, "Ausgezählt", c't 2023, Heft 28, S. 142 ff.](https://www.heise.de/select/ct/2023/28/2329609540558917729)
+* [Kasiski-Test und weitere Materialien: ct.de/yzr6](https://ct.de/yzr6)
---
+++ kasiski.hs
@@ -0,0 +1,46 @@
+import Data.Char
+import Data.List
+
+-- calculate the encryption key with the length n from the cipher text
+uncrypt :: Int -> String -> String
+uncrypt n = map (cipherChar . findMostFrequentChar) . sliceHorizontal n
+
+-- stolen from utility-ht-0.0.17: Various small helper functions for Lists, Maybes, Tuples, Functions
+sliceHorizontal :: Int -> String -> [String]
+sliceHorizontal n = map (sieve n) . take n . iterate (drop 1)
+  where
+    sieve :: Int -> [a] -> [a]
+    sieve k = unfoldr (\xs -> toMaybe (not (null xs)) (head xs, drop k xs))
+  
+    toMaybe :: Bool -> a -> Maybe a
+    toMaybe False _ = Nothing
+    toMaybe True  x = Just x
+
+-- find the most frequent character
+findMostFrequentChar :: String -> Char
+findMostFrequentChar = snd . maximum . map (\xs -> (length xs, head xs)) . group . sort
+
+-- Assuming the letter 'E' as to most frequent character in this alphabet
+-- calculate the cipher character assuming its position relative to the character 'E'
+cipherChar :: Char -> Char
+cipherChar c = let x = ord c - ord 'E'
+               in if x < 0 then chr (ord 'A' + x + 26) else chr (ord 'A' + x)
+
+{-
+plain :: String
+plain = filter isAlpha $ map toUpper $ concat
+  ["Man verabredet mit demjenigen, mit",
+    "welchem man im Geheimen correspondieren",
+    "will, fuer jeden Buchstaben eine besondere Chiffre und schreibt",
+    "dann mit diesen Chiffren in der",
+    "gewoehnlichen Art."]
+  
+cipher :: String
+cipher = encode "UNIX" plain
+
+plain = "MANVERABREDETMITDEMJENIGENMITWELCHEMMANIMGEHEIMENCORRESPONDIERENWILLFUERJEDENBUCHSTABENEINEBESONDERECHIFFREUNDSCHREIBTDANNMITDIESENCHIFFRENINDERGEWOEHNLICHENART"
+cipher = "GNVSYEIYLRLBNZQQXRUGYAQDYAUFNJMIWUMJGNVFGTMEYVUBHPWOLRAMIALFYEMKQVTIZHMODRLBHOCZBFBXVRVBCAMYYFWKXRZBWUQCZEMRHQAZBEMFVGLXHAUFNQQBMRVZBVNCLRVFHQMOARELYUVICPPBHNZQ"
+
+uncrypt 4 cipher
+"UNIX"
+-}
