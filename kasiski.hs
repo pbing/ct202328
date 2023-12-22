41697c7fe3a040aff7a17759588b454ea109a8ba
Bernd Beuster <bernd.beuster@gmail.com>
2023-12-22 20:58:38 +0100

    Add autocorrelation

--- kasiski.hs
+++ kasiski.hs
@@ -11,7 +11,7 @@ sliceHorizontal n = map (sieve n) . take n . iterate (drop 1)
   where
     sieve :: Int -> [a] -> [a]
     sieve k = unfoldr (\xs -> toMaybe (not (null xs)) (head xs, drop k xs))
-  
+
     toMaybe :: Bool -> a -> Maybe a
     toMaybe False _ = Nothing
     toMaybe True  x = Just x
@@ -26,6 +26,18 @@ cipherChar :: Char -> Char
 cipherChar c = let x = ord c - ord 'E'
                in if x < 0 then chr (ord 'A' + x + 26) else chr (ord 'A' + x)
 
+-- print an autocorrelation histogram in order to detect the length of the encryption key
+printHistogram :: [Int] -> IO ()
+printHistogram = putStrLn . unlines . map (`replicate` '*')
+
+autocorrelate :: String -> [Int]
+autocorrelate xs = map go [0 .. length xs - 1]
+  where
+    go n = sum $ map fromEnum $ zipWith (==) xs (rotate n xs)
+
+rotate :: Int -> [a] -> [a]
+rotate n xs = take (length xs) $ drop n $ cycle xs
+
 {-
 plain :: String
 plain = filter isAlpha $ map toUpper $ concat
@@ -34,12 +46,14 @@ plain = filter isAlpha $ map toUpper $ concat
     "will, fuer jeden Buchstaben eine besondere Chiffre und schreibt",
     "dann mit diesen Chiffren in der",
     "gewoehnlichen Art."]
-  
+
 cipher :: String
 cipher = encode "UNIX" plain
 
 plain = "MANVERABREDETMITDEMJENIGENMITWELCHEMMANIMGEHEIMENCORRESPONDIERENWILLFUERJEDENBUCHSTABENEINEBESONDERECHIFFREUNDSCHREIBTDANNMITDIESENCHIFFRENINDERGEWOEHNLICHENART"
 cipher = "GNVSYEIYLRLBNZQQXRUGYAQDYAUFNJMIWUMJGNVFGTMEYVUBHPWOLRAMIALFYEMKQVTIZHMODRLBHOCZBFBXVRVBCAMYYFWKXRZBWUQCZEMRHQAZBEMFVGLXHAUFNQQBMRVZBVNCLRVFHQMOARELYUVICPPBHNZQ"
+
+printHistogram $ drop 1 $ autocorrelate cipher
 
 uncrypt 4 cipher
 "UNIX"
